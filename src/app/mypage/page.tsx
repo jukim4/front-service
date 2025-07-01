4db7c365a4dc7963c328371fa4c7ae6362e4eb98
juyeongkim <kimjuyeong2@gmail.com>
2025-07-01 21:41:03 +0900

    add: 권한 없는 사용자에 경우 페이지 리다이렉트

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,5 +1,21 @@
+'use client';
+
 import MyPage from '@/components/MyPage';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuthStore } from '@/store/authStore';
+
 export default function MyPageScreen() {
+  const router = useRouter();
+  useEffect(() => {
+    if(!useAuthStore.getState().isAuthenticated) {
+      // 인증되지 않은 경우 로그인 페이지로 리다이렉트
+      router.push('/login');
+    } else {
+      router.replace('/portfolio/holdings');
+    }
+  }, []);
+
   return <MyPage />;
 }
