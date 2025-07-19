9018929d947c3a65fa26f976323c90cedd9e721b
JaeHwan Kim <iwantbaobab@gmail.com>
2025-07-19 12:32:20 +0900

    Publishing(#59): 보유자산 창에서 보유원화도 확인할 수 있도록 퍼블리싱

--- src/components/TotalBuyCoin.tsx
+++ src/components/TotalBuyCoin.tsx
@@ -27,43 +27,51 @@ export default function TotalBuyCoin() {
       <div className="grid grid-cols-2 gap-8 p-8 rounded-lg bg-blue-50 border max-w-screen-lg mx-auto">
         {/* 왼쪽 */}
         <div className="space-y-6">
-          <div className="flex flex-col space-y-1">
-            <span className="text-base text-gray-600 font-medium">총 매수 금액</span>
-            <span className="text-3xl font-bold text-black">
-              {(formatNumber(safeNumber(totalBuy)))}
-            </span>
-          </div>
-
-          <div className="flex justify-between">
-            <span className="text-base text-gray-600 font-medium">총 평가 금액</span>
-            <span className="text-lg font-semibold text-black">
-              {formatNumber(safeNumber(totalValuation))}
-            </span>
-          </div>
-        </div>
-
-        {/* 오른쪽 */}
-        <div className="space-y-6">
-          <div className="flex flex-col space-y-1">
+        <div className="flex flex-col space-y-1">
             <span className="text-base text-gray-600 font-medium">총 보유 자산</span>
             <span className="text-3xl font-bold text-black">
               {formatNumber(safeNumber(totalAsset))}
             </span>
           </div>
 
-          <div className="flex justify-between">
-            <span className="text-base text-gray-600 font-medium">총 평가 손익</span>
-            <span className={`text-lg font-semibold ${profit < 0 ? "text-blue-500" : "text-red-500"}`}>
-              {formatNumber(safeNumber(profit))}
+          <div className="flex flex-col space-y-1">
+            <span className="text-base text-gray-600 font-medium">총 보유 원화 자산</span>
+            <span className="text-3xl font-bold text-black">
+              {(formatNumber(safeNumber(holdings)))}
             </span>
           </div>
+        </div>
 
-          <div className="flex justify-between">
-            <span className="text-base text-gray-600 font-medium">총 평가 수익률</span>
-            <span className={`text-lg font-semibold ${profit < 0 ? "text-blue-500" : "text-red-500"}`}>
-              {formatNumber(safeNumber(profitRate), 2)} %
+        {/* 오른쪽 */}
+        <div className="space-y-6">
+         <div className="flex flex-col space-y-1">
+            <span className="text-base text-gray-600 font-medium">총 매수 금액</span>
+            <span className="text-3xl font-bold text-black">
+              {(formatNumber(safeNumber(totalBuy)))}
             </span>
           </div>
+          <div className="space-y-1">
+            <div className="flex justify-between">
+              <span className="text-base text-gray-600 font-medium">총 평가 손익</span>
+              <span className={`text-lg font-semibold ${profit < 0 ? "text-blue-500" : "text-red-500"}`}>
+                {formatNumber(safeNumber(profit))}
+              </span>
+            </div>
+
+            <div className="flex justify-between">
+              <span className="text-base text-gray-600 font-medium">총 평가 금액</span>
+              <span className={`text-lg font-semibold ${profit < 0 ? "text-blue-500" : "text-red-500"}`}>
+                {formatNumber(safeNumber(totalValuation))}
+              </span>
+            </div>
+
+            <div className="flex justify-between">
+              <span className="text-base text-gray-600 font-medium">총 평가 수익률</span>
+              <span className={`text-lg font-semibold ${profit < 0 ? "text-blue-500" : "text-red-500"}`}>
+                {formatNumber(safeNumber(profitRate), 2)} %
+              </span>
+            </div>
+        </div>
         </div>
       </div>
     </div>
