d04432509d1f65f3a7e04075f4190c75ce9c42a3
JaeHwan Kim <iwantbaobab@gmail.com>
2025-07-23 15:31:22 +0900

    Chore(#69): 변경된 환경변수 이름 최신화

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     // 환경별 웹소켓 URL 설정
-    UPBIT_WS_URL: process.env.UPBIT_WS_URL || 'wss://api.upbit.com/websocket/v1',
+    WS_URL: process.env.WS_URL || 'wss://api.upbit.com/websocket/v1',
     UPBIT_API_URL: process.env.UPBIT_API_URL || 'https://api.upbit.com/v1',
   },
   compiler: {
