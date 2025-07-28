f9325009ed0776f5b490dce02d46f2750838850d
juyeongkim <wkdwn0414@naver.com>
2025-07-28 15:28:35 +0900

    modify: 확실한 오류

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,19 @@
-// test.tsx
-const userCode = "console.log('Hello')";
-eval(userCode); // 🚨 Semgrep p/javascript.security.audit.eval-detect 잡힘
+function test1(req,res) {
+  const data = JSON.stringify(req.query.key);
+  const command = `(secret) => {${data}}`
+  // ruleid:code-string-concat
+  return eval(command)
+}
+
+test2.post(foo, bar, function (req,res) {
+  userInput = req.params.input
+  var command = "new Function('"+userInput+"')";
+  // ruleid:code-string-concat
+  return eval(command)
+});
+
+function ok1(req,res) {
+  var command = "eval('123')";
+  // ok:code-string-concat
+  return eval(command)
+}
